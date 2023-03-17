bd6fb4bd5ec0c66c339885b2847c5c6aa35fd803
ExpensiveLadder <68307915+ExpensiveLadder@users.noreply.github.com>
2023-03-18 09:31:28 +1300

    blacklist uses formkeys

--- SustainedSpellsPatcher/Program.cs
+++ SustainedSpellsPatcher/Program.cs
@@ -19,6 +19,8 @@ namespace SustainedSpellsPatcher
         [SettingName("Blacklisted FormKeys")]
         public List<string> blacklist = new();
 
+        public List<IFormLinkGetter<ISpellGetter>> blacklist2 = new();
+
         //public int maxSummons = 2;;
     }
 
